cf9632ab470b1221771251621bdba99809df1f4d
Satoru Takahashi <b1020033@fun.ac.jp>
2026-06-29 23:32:30 +0900

    fix: Google Material Symbolsをlayout.tsxのlinkタグで読み込むように修正し表示バグを解決

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" className={`${inter.variable} antialiased`}>
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </head>
       <body className="min-h-screen bg-nf-black text-nf-white">
         {children}
       </body>
